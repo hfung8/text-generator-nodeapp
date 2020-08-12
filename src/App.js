f23ba50fa395358a32608eb70712af7b69ef8ca4
Harrison Fung <harrison.hok.fung@gmail.com>
2020-08-12 08:39:17 -0400

    added components output and select to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Output from './components/Output';
+import Select from'./components/Controls/Select';
 import './App.css';
 
 class App extends Component {
@@ -7,28 +9,39 @@ constructor(props){
   super(props);
   this.state = {
     paras: 4,
-    html: true,
     text: ''
   }
 }
 
 componentWillMount(){
-  this.getSampleText();\
+  this.getSampleText();
 }
 
 getSampleText(){
-  axios.get('http://hispterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+  axios.get('https://baconipsum.com/api/?type=all-meat&paras='+this.state.paras+'&start-with-lorem=1&format=text')
     .then((response) => {
-      this.setState({text: response.data.text}), function() {
+      this.setState({text: response.data}, function(){
         console.log(this.state);
-      }
+      });
     })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+
 
   render(){
   return (
-    <div className="App">
-      Hello
+    <div className="App container">
+      <h1>React js Sample Text Generator</h1>
+      <hr />
+      <form class="form-inline">
+        <div class="form-group">
+          <label>Include HTML:</label>
+          <Select value={this.state.html} />
+        </div>
+      </form>
+      <Output value={this.state.text} />
     </div>
   );
   }
